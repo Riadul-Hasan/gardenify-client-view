d00d05b019237c7887ba1367d0e7f9d22fb2dbb4
Riadul-Hasan <riadb468@gmail.com>
2025-05-24 21:53:32 +0600

    navigate use in share tip success

--- src/pages/ShareGardenTip.jsx
+++ src/pages/ShareGardenTip.jsx
@@ -1,9 +1,11 @@
 import React, { use } from 'react';
 import { AuthContext } from '../provider/AuthProvider';
 import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router';
 
 const ShareGardenTip = () => {
     const { user } = use(AuthContext)
+    const navigate = useNavigate()
 
     const handleAddTips = (e) => {
         e.preventDefault()
@@ -32,6 +34,7 @@ const ShareGardenTip = () => {
                         icon: "success",
                         draggable: true
                     });
+                    navigate("/browseTips")
                 }
             })
 
@@ -50,82 +53,75 @@ const ShareGardenTip = () => {
 
 
 
-                <form data-theme="light" onSubmit={handleAddTips} className=' p-8 bg-base-200 border-2 border-green-500 rounded-2xl'>
-                    <div className='grid grid-cols-1 md:grid-cols-2 gap-4 '>
-                        <fieldset className="fieldset bg-white border-black rounded-box  border p-4">
 
+
+                <form data-theme="light" onSubmit={handleAddTips} className='p-4 md:p-8 bg-base-200 border-2 border-green-500 rounded-2xl'>
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                        <fieldset className="fieldset bg-white border-black rounded-box border p-4 md:col-span-1">
                             <label className="label">Title</label>
                             <input type="text" className="input w-full" name='title' placeholder="Title here" />
                         </fieldset>
-                        <fieldset className="fieldset bg-white border-black rounded-box  border p-4">
 
+
+                        <fieldset className="fieldset bg-white border-black rounded-box border p-4 md:col-span-1">
                             <label className="label">Plant Type</label>
                             <input type="text" className="input w-full" name='plantType' placeholder="Write here" />
                         </fieldset>
-                        <fieldset className="fieldset bg-white border-black rounded-box  border p-4">
 
+
+                        <fieldset className="fieldset bg-white border-black rounded-box border p-4 md:col-span-1">
                             <label className="label">Difficulty Level</label>
-                            <select name="difficulty" className='bg-white border rounded-xl border-gray-400 p-4' id="">
-
+                            <select name="difficulty" className='bg-white border rounded-xl border-gray-400 p-4 w-full'>
                                 <option value="easy">Easy</option>
                                 <option value="medium">Medium</option>
                                 <option value="hard">Hard</option>
                             </select>
                         </fieldset>
 
-                        <fieldset className="fieldset bg-white md:col-spa border-black rounded-box  border p-4">
 
+                        <fieldset className="fieldset bg-white border-black rounded-box border p-4 md:col-span-1">
                             <label className="label">Category</label>
-                            <select name="category" className='border rounded-xl border-gray-400 p-4' id="">
-
+                            <select name="category" className='border rounded-xl border-gray-400 p-4 w-full'>
                                 <option value="composting">Composting</option>
                                 <option value="plantCare">Plant Care</option>
                                 <option value="vartecalGardening">Vertical Gardening</option>
                             </select>
                         </fieldset>
-                        <fieldset className="fieldset bg-white  border-black rounded-box  border p-4 col-span-2">
 
+
+                        <fieldset className="fieldset bg-white border-black rounded-box border p-4 col-span-1 md:col-span-2">
                             <label className="label">Description</label>
-                            <textarea className='bg-white border rounded-xl border-gray-400' name="description" id="" cols="10" rows="6"></textarea>
+                            <textarea className='bg-white border rounded-xl border-gray-400 w-full' name="description" cols="10" rows="6"></textarea>
                         </fieldset>
 
-                        <fieldset className="fieldset bg-white border-black rounded-box  border p-4 ">
 
+                        <fieldset className="fieldset bg-white border-black rounded-box border p-4 md:col-span-1">
                             <label className="label">Image Url</label>
                             <input type="text" className="input w-full" name='imageUrl' placeholder="URL" />
                         </fieldset>
 
 
-
-                        <fieldset className="fieldset bg-base-200 border-black rounded-box  border p-4">
-
+                        <fieldset className="fieldset bg-base-200 border-black rounded-box border p-4 md:col-span-1">
                             <label className="label">Availability</label>
-                            <select name="availability" className='bg-white p-3 border rounded-xl border-gray-400' id="">
-
+                            <select name="availability" className='bg-white p-3 border rounded-xl border-gray-400 w-full'>
                                 <option value="public">Public</option>
                                 <option value="hidden">Hidden</option>
-
                             </select>
                         </fieldset>
 
-                        <fieldset className="fieldset bg-base-200 border-black rounded-box  border p-4">
 
+                        <fieldset className="fieldset bg-base-200 border-black rounded-box border p-4 md:col-span-1">
                             <label className="label">Email</label>
                             <input type="email" className="input w-full" readOnly name='email' defaultValue={user.email} />
                         </fieldset>
 
-                        <fieldset className="fieldset bg-base-200 border-black rounded-box  border p-4">
-
+                        <fieldset className="fieldset bg-base-200 border-black rounded-box border p-4 md:col-span-1">
                             <label className="label">Name</label>
                             <input type="text" className="input w-full" readOnly name='name' defaultValue={user.displayName} />
                         </fieldset>
                     </div>
 
-
-
-
-
-                    <div className='py-10'>
+                    <div className='py-6 md:py-10'>
                         <input type="submit" className='btn w-full bg-green-600 text-white' value="Submit" />
                     </div>
                 </form>
